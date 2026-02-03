1b6bbaf73cca0e0f584026b9ca656dd1b7159e49
harrywoodhouse <woodhouses.harry@gmail.com>
2026-02-03 12:01:47 +0000

    Test functions in lib.rs

--- praat-sys/src/lib.rs
+++ praat-sys/src/lib.rs
@@ -1,1 +1,28 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_praat_identity() {
+        unsafe {
+            let version = Melder_appVersion();
+            println!("Praat Melder_appVersion returned: {}", version);
+            assert!(version >= 0);
+        }
+    }
+
+    #[test]
+    fn test_melder_play_exists() {
+        unsafe {
+            let func_ptr = Melder_play as *const ();
+            assert!(!func_ptr.is_null());
+            println!("Linker successfully resolved Melder_play at {:?}", func_ptr);
+        }
+    }
+}
