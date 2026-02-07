65b8bbf37a12ca700b4510f9500ddd8697073fe0
harrywoodhouse <woodhouses.harry@gmail.com>
2026-02-07 04:27:56 +0000

    gsl, dlmach and num stubs

--- praat-sys/src/stubs.cpp
+++ praat-sys/src/stubs.cpp
@@ -14,9 +14,8 @@ RUSTMOUTH. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
-
-// this hopefully cancels the praat GUI and means I don't have to manually comment out
-// headers in the praat source code
+#include <string.h> 
+#include "Sound.h"
 
 extern "C" {
     void Graphics_ps_init() {}
@@ -25,5 +24,25 @@ extern "C" {
     
     int Melder_isGuiAvailable() {
         return 0; 
+    }
+
+    // These may move around but they were used to really establish a link 
+
+    structSound* rustmouth_Sound_create(long long channels, double xmin, double xmax, long long nx, double dx, double x1) {
+        autoSound sound = Sound_create(channels, xmin, xmax, nx, dx, x1);
+        structSound* rawPtr = *(structSound**)&sound;
+        memset(&sound, 0, sizeof(sound));
+        return rawPtr;
+    }
+
+    double rustmouth_Sound_getSample(structSound* sound, long long channel, long long sample_index) {
+        if (!sound) return 0.0;
+        return sound -> z [channel] [sample_index];
+    }
+
+    void rustmouth_Sound_setSample(structSound* sound, long long channel, long long sample_index, double value) {
+        if (sound) {
+            sound -> z [channel] [sample_index] = value;
+        }
     }
 }
